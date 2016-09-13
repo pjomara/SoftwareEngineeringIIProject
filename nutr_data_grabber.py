5a3f4ade9baff77a1dc89e2109d0d0754bdef1f6
pjomara <pomar001@plattsburgh.edu>
2016-09-13 11:40:39 -0400

    Update nutr_data_grabber.py

--- nutr_data_grabber.py
+++ nutr_data_grabber.py
@@ -1,14 +1,31 @@
+'''This is a command-line program that asks for ingredient amount, unit, and name.
+Compiles each ingredient in a .txt fiel named recipe_book.txt.
+Queries an ingredient database, pulls nutritional info. for each ingredient.
+Prints cumulative nutritional info. to recipe_book.txt.'''
+
 import sqlite3
 
 def main():
-    ingredient = input("Enter the ingredient: ")
-    if ingredient:
-        description, calories, protein, total_fat, carbohydrates= nutr_grabber(ingredient)
-        print (description)
-        print (calories)
-        print (protein)
-        print (total_fat)
-        print (carbohydrates)
+    title= input("Enter the recipe title: ")
+    title_write(title)
+    more= 'yes'
+    tot_calories= float(0)
+    tot_protein= float(0)
+    tot_fat= float(0)
+    tot_carb= float(0)
+    while more == 'yes':
+        amount = eval(input("Enter amount: "))
+        unit = input("Enter the unit(I.E.- cup, tsp); ")
+        ingredient = input("Enter the ingredient: ")
+        if ingredient:
+            description, calories, protein, total_fat, carbohydrates= nutr_grabber(ingredient)
+            recipe_write(amount, unit, description)
+            tot_calories= tot_calories + float(calories)
+            tot_protein = tot_protein + float(protein)
+            tot_fat = tot_fat + float(total_fat)
+            tot_carb = tot_carb + float(carbohydrates)
+            more = input("More ingredients? (Enter 'yes' or 'no'):")
+    nutr_write(tot_calories, tot_protein, tot_fat, tot_carb)
 
 def nutr_grabber(ingredient):
     conn= sqlite3.connect('USDADataProto.db')
@@ -19,11 +36,11 @@ def nutr_grabber(ingredient):
 
             row= c.fetchone()
             if row:
-                description = row[1]
-                calories= row[3]
-                protein= row[4]
-                total_fat= row[5]
-                carbohydrates= row[7]
+                description = row[0]
+                calories= row[1]
+                protein= row[2]
+                total_fat= row[3]
+                carbohydrates= row[4]
             else:
                 description = None
                 calories= None
@@ -38,5 +55,44 @@ def nutr_grabber(ingredient):
         conn.close()
 
     return description, calories, protein, total_fat, carbohydrates
+
+#prints title of recipe to recipe_book.txt
+def title_write(title):
+    recipe= open("recipe_book.txt", "a")
+    print (title, file=recipe)
+    recipe.close()
+
+#prints amount, unit, and ingredient to recipe_book.txt
+def recipe_write(amount, unit, description):
+    recipe= open("recipe_book.txt", "a")
+    print (amount,' ', unit,' ',description, file=recipe)
+    recipe.close()
+
+#prints nutritional information to recipe_book.txt
+#as of 9/13/2016- prints per 100g for each ingredient.- This needs to be fixed    
+def nutr_write(tot_calories, tot_protein, tot_fat, tot_carb):
+    recipe= open("recipe_book.txt", "a")
+    print ("\n","Nutritional information", file=recipe)
+    print ("calories: ",round(tot_calories,2), file=recipe)
+    print ("Protein: ", round(tot_protein, 2), file=recipe)
+    print ("Fat: ", round(tot_fat, 2), file=recipe)
+    print ("Carbohydrates: ", round(tot_carb, 2), file=recipe)
+    recipe.close()
+
+# cal_converter converts calories from per 100g to recipe amount. 
+def cal_converter():
+    pass
+
+#prot_converter converts protien from per 100 g to recipe amount.
+def prot_converter():
+    pass
+
+#fat_converter converts fat from per 100g to recipe amount.
+def fat_converter():
+    pass
+
+#carb_converter converts carbs from per 100g to recipe amount.
+def carb_converter():
+    pass
 
 main()
