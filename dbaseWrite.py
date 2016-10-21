6a38c001fabcd05031978a90faeacf0ea9f27ad6
Parker O'Mara <pomar001atplattsburgh.edu>
2016-10-21 11:39:43 -0400

    added id_grabber() to dbaseWriter.py

--- dbaseWrite.py
+++ dbaseWrite.py
@@ -3,6 +3,9 @@ import sqlite3
 def main():
     title= input("Enter recipe title: ")
     recipe_write_dbase(title)
+    recipe_id= id_grabber(title)
+    print (recipe_id)
+    
 
 def recipe_write_dbase(title):
     conn= sqlite3.connect("recipe.sqlite")
@@ -11,10 +14,39 @@ def recipe_write_dbase(title):
         try:
             c.execute("INSERT INTO recipe (recipe_title) VALUES (?)", (title,))
             conn.commit()
+
         finally:
             c.close()
 
     finally:
         conn.close()
+
+def id_grabber(title):
+    conn= sqlite3.connect("recipe.sqlite")
+    try:
+        c= conn.cursor()
+        try:
+            c.execute("SELECT recipe_id FROM recipe where recipe_title = ?", (title,))
+
+            row= c.fetchone()
+            if row:
+                recipe_id= row[0]
+            else:
+                recipe_id= None
+        finally:
+            c.close()
+
+    finally:
+        conn.close()
+
+    return recipe_id
+
+'''def ingredient_write_dbase(index, amount, unit, converted_ingr):
+    conn=sqlite3.connect("recipe.sqlite")
+    try:
+        c=conn.cursor()
+        try:
+            c.execute("SELECT FROM recipe'''
+
 
 main()
