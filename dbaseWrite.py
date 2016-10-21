e9aa29b6f516fafe208985d475f9fed38921c20f
Parker O'Mara <pomar001atplattsburgh.edu>
2016-10-21 14:29:33 -0400

    commit of edits made while GitHub was down

--- dbaseWrite.py
+++ dbaseWrite.py
@@ -1,8 +1,4 @@
 import sqlite3
-'''This script contains the functions for writing recipes' contents to a database for later retrieval.
-This uses two sqlite databases, recipe.sqlite and ingredients.sqlite.
-
-This code will eventually be incorporated into nutr_app.py''' 
 
 def main():
     title= input("Enter recipe title: ")
@@ -10,7 +6,7 @@ def main():
     recipe_id= id_grabber(title)
     print (recipe_id)
     
-'''Opens recipe.sqlite, writes recipe title and databse generates recipe_id.   '''
+
 def recipe_write_dbase(title):
     conn= sqlite3.connect("recipe.sqlite")
     try:
@@ -25,7 +21,6 @@ def recipe_write_dbase(title):
     finally:
         conn.close()
 
-'''Returns recipe_id to be used to link specific ingredients with a recipe.'''
 def id_grabber(title):
     conn= sqlite3.connect("recipe.sqlite")
     try:
@@ -46,14 +41,21 @@ def id_grabber(title):
 
     return recipe_id
 
-'''Will write ingredient specifics to ingredients.sqlite (not created yet).
-Each ingredient has the recipe_id attached for later retrieval and compiling.'''
-'''def ingredient_write_dbase(index, amount, unit, converted_ingr):
-    conn=sqlite3.connect("recipe.sqlite")
+def ingredient_write_dbase(recipe_id, amount, unit, converted_ingr):
+    conn= sqlite3.connect("ingredients.sqlite")
     try:
         c=conn.cursor()
         try:
-            c.execute("SELECT FROM recipe'''
+            c.execute("INSERT INTO ingredients (recipe_id, amount, unit, converted_ingr)\
+                      VALUES (?, ?, ?, ?)", (recipe_id, amount, unit, converted_ing,))
+            conn.commit()
+        finally:
+            c.close()
+    finally:
+        conn.close()
+
+
+
 
 
 main()
