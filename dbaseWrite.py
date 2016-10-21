6eacd059b3d656ff40379672328c0f8eac0e53c6
Parker O'Mara <pomar001atplattsburgh.edu>
2016-10-21 11:49:43 -0400

    added comments to dbaseWrite.py

--- dbaseWrite.py
+++ dbaseWrite.py
@@ -1,4 +1,8 @@
 import sqlite3
+'''This script contains the functions for writing recipes' contents to a database for later retrieval.
+This uses two sqlite databases, recipe.sqlite and ingredients.sqlite.
+
+This code will eventually be incorporated into nutr_app.py''' 
 
 def main():
     title= input("Enter recipe title: ")
@@ -6,7 +10,7 @@ def main():
     recipe_id= id_grabber(title)
     print (recipe_id)
     
-
+'''Opens recipe.sqlite, writes recipe title and databse generates recipe_id.   '''
 def recipe_write_dbase(title):
     conn= sqlite3.connect("recipe.sqlite")
     try:
@@ -21,6 +25,7 @@ def recipe_write_dbase(title):
     finally:
         conn.close()
 
+'''Returns recipe_id to be used to link specific ingredients with a recipe.'''
 def id_grabber(title):
     conn= sqlite3.connect("recipe.sqlite")
     try:
@@ -41,6 +46,8 @@ def id_grabber(title):
 
     return recipe_id
 
+'''Will write ingredient specifics to ingredients.sqlite (not created yet).
+Each ingredient has the recipe_id attached for later retrieval and compiling.'''
 '''def ingredient_write_dbase(index, amount, unit, converted_ingr):
     conn=sqlite3.connect("recipe.sqlite")
     try:
