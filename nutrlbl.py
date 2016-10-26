98d0985dea95d0fa576947b20d93651f01f6a556
ebeam001 <ebeam001@plattsburgh.edu>
2016-10-26 12:10:06 -0400

    Alternate Nutrition Label

--- nutrlbl.py
+++ nutrlbl.py
@@ -9,14 +9,25 @@ class line:
 
 serv_size= 13
 cal= 250
-fat= 10
-sat= 3
-tran= 0
-chol= 0
-sod=123
-carb=22
-sug=10
-prot=4
+fat= 10.0
+sat= 3.0
+tran= 7.0
+chol= 100.0
+sod=123.0
+carb=22.0
+sug=10.0
+prot=4.0
+tot_ft_dv =65.0
+tot_chol_dv =300.0
+tot_sod_dv =2400.0
+tot_carb_dv =300.0
+tot_prot_dv =50.0
+
+ft_dv= (fat/tot_ft_dv)*100
+chol_dv= (chol/tot_chol_dv)*100
+sod_dv= (sod/tot_sod_dv)*100
+carb_dv= (carb/tot_carb_dv)*100
+prot_dv= (prot/tot_prot_dv)*100
 
 print "\n"
 print "\n"
@@ -24,20 +35,27 @@ print "___________________________________"
 print line.BOLD + "Nutrition Facts" + line.END
 print "Serving Size ", serv_size, "g"
 print line.UNDERLINE + "___________________________________" + line.END
-print line.UNDERLINE + "Amount Per Serving                 " + line.END
-print line.UNDERLINE + "Calories ", cal, "                     " + line.END
-print line.UNDERLINE + "                     % Daily Value*" + line.END
-print line.UNDERLINE + "Total Fat ", fat, "g                    " + line.END
-print line.UNDERLINE + "   Saturated Fat ", sat, "g              " + line.END
-print line.UNDERLINE + "   Trans Fat ", tran, "g                  " + line.END
-print line.UNDERLINE + "Cholesterol ", chol, "mg                  " + line.END
-print line.UNDERLINE + "Sodium ", sod, "mg                     " + line.END
-print line.UNDERLINE + "Total Carbohydrate ", carb, "g           " + line.END
-print line.UNDERLINE + "   Sugars ", sug, "g                    " + line.END
-print line.UNDERLINE + "Protein ", prot, "g                       " + line.END
+print "Amount Per Serving"
+print "___________________________________"
+print "Calories ", cal
+print "                     % Daily Value*"
+print "___________________________________"
+print "Total Fat ", fat, "g           ", round(ft_dv),"%"
+print "___________________________________"
+print "   Saturated Fat ", sat, "g            "
+print "___________________________________"
+print "   Trans Fat ", tran, "g                "
+print "___________________________________"
+print "Cholesterol ", chol, "mg       ", round(chol_dv), "%"
+print "___________________________________"
+print "Sodium ", sod, "mg             ", round(sod_dv), "%"
+print "___________________________________"
+print "Total Carbohydrate ", carb, "g   ", round(carb_dv), "%"
+print "___________________________________"
+print "   Sugars ", sug, "g                  "
+print "___________________________________"
+print "Protein ", prot, "g               ", round(prot_dv), "%"
 print line.UNDERLINE + "___________________________________" + line.END
 print "*Percent Daily Values are based on"
-print " a 2,000 calorie diet. Your daily"
-print " values may be higher or lower "
-print line.UNDERLINE + " depending on your calorie needs.  " + line.END
+print line.UNDERLINE + " a 2,000 calorie diet.             " + line.END
 print "\n"
