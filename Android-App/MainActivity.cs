a9fb81d6a2624ccb3849a8bc1fb73368e9a3ad26
slest003 <slest003@plattsburgh.edu>
2016-10-26 17:48:32 -0400

    Update MainActivity.cs

--- Android-App/MainActivity.cs
+++ Android-App/MainActivity.cs
@@ -1,9 +1,8 @@
-//Added a Unit Spinner so that users cannot enter an un-usable unit.
-//Added an auto-complete ingredient selection so that users can see the different options for their ingredient
-//and have it properly formatted.
-//Also added a new text view (it just makes it looka  little nicer.)
+//starting to implement unit calculations. Currently only have if the database unit is the same as the entered unit, and cup to tbsp.
+// Also fixed the spinner_itmselected so it returns the correct unit and is now usable.
 
-uusing System;
+
+using System;
 using Android.Views;
 using Android.Content;
 using Android.Runtime;
@@ -18,17 +17,35 @@ using Android.Database.Sqlite;
 using System.Collections.Generic;
 using System.Collections;
 
+
 namespace nutr_grabber
 {
 
     [Activity(Label = "nutr_grabber", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        string str1;
-      
-        
-        
-                // Android needs a database to be copied from assets to a useable location in inernal memory
+        string str1; 
+        string unitEntered;
+        double tspInCup = 48.0;
+        double tbspInCup = 16.0;
+        double tspInTbsp = 3.0;
+        double flozInCup = 8.0;
+        double flozInTbsp = 0.5;
+        double ozInLb = 16.0;
+
+        string name;
+        double cal;
+        double protein;
+        double fat;
+        double carbs;
+        double sodium;
+        double sugar;
+        double num;
+        string unit;
+
+
+
+        // Android needs a database to be copied from assets to a useable location in inernal memory
         public void copyDataBase()
         {
             var dbPath = System.IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "USDADataProto.db");
@@ -52,12 +69,12 @@ namespace nutr_grabber
                 dbAssetStream.Close();
             }
         }
+        
         // gets item from spinner
         private void unitSpin_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
         {
             Spinner spinner = (Spinner)sender;
-
-            string toast = string.Format("{0}", spinner.GetItemAtPosition(e.Position));
+            unitEntered = string.Format("{0}", spinner.GetItemAtPosition(e.Position));
             
         }
 
@@ -93,12 +110,14 @@ namespace nutr_grabber
             adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
             unitSpin.Adapter = adapter;
 
-            //************ will get the selected unit from spinner. will use shortly, but not yet
+            // get the selected unit from spinner
             unitSpin.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(unitSpin_ItemSelected);
+            
 
-            //this chunk of code is used to create the actual auto-complete text.
+
+            //this chunk of code is used to create the actual auto-text.
             //I create a new, blank array. Then select the entire table I created earlier.
-            //Then, for every row of info, add only the Shrt_Desc (the name of the ingredient)
+            //Then, for every row of info add only the Shrt_Desc (the name of the ingredient)
             // to the blank array. 
             ArrayList autoIngs = new ArrayList();
             var addToAuto = db.Table<USDADataProto>();
@@ -107,24 +126,67 @@ namespace nutr_grabber
                 autoIngs.Add(auto.Shrt_Desc);
              }
              //When the text in the text-box changes, populate the auto-complete suggestions 
-             // with the name of the ingredients from the array.
+             // with the name of the ingredients from the array
             autoIngred.TextChanged += delegate
             {
                 ArrayAdapter autoCompAdapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleDropDownItem1Line, autoIngs);
                 autoIngred.Adapter = autoCompAdapter;
             };
 
+
+
+
             //when you click the search button
             search.Click += (object sender, EventArgs e) =>
                     {
-                        //formats input text to use correctly
+                        //formats input text. TrimEnd takes the space off the end.
+                        //If the space is there it causes a crash. The datbase is also all caps.
                         str1 = autoIngred.Text;
                         str1 = str1.TrimEnd();
                         str1 = str1.ToUpper();
 
+                        double amountEntered = Convert.ToDouble(amount.Text);
+                        
 
                         // make list from a query in the USDADataProto table. Selects everything in the row where shrt_desc is equal to str1
                         List<USDADataProto> query = db.Query<USDADataProto>("SELECT * FROM USDADataProto WHERE Shrt_Desc = ?", str1);
+
+                        foreach (var i in query)
+                        {
+                            name = i.Shrt_Desc;
+                            cal = i.Energ_Kcal;
+                            protein = i.Protein_g;
+                            fat = i.Lipid_Tot_g;
+                            carbs = i.Carbohydrt_g;
+                            sodium = i.Sodium_mg;
+                            sugar = i.Sugar_Tot_g;
+                            num = i.num;
+                            unit = i.unit;
+                        }
+
+
+                        if (unit == unitEntered)
+                            {
+                                cal = cal * amountEntered;
+                                protein = protein * amountEntered;
+                                fat = fat * amountEntered;
+                                carbs = carbs * amountEntered;
+                                sodium = sodium * amountEntered;
+                                sugar = sugar * amountEntered;
+                            }
+                            else if(unit == "cup" && unitEntered == "tbsp")
+                            {
+                                cal = (cal / tbspInCup) * amountEntered;
+                                protein = (protein / tbspInCup) * amountEntered;
+                                fat = (fat / tbspInCup) * amountEntered;
+                                carbs = (carbs / tbspInCup) * amountEntered;
+                                sodium = (sodium / tbspInCup) * amountEntered;
+                                sugar = (sugar / tbspInCup) * amountEntered;
+                            }
+
+                        
+
+
 
 
                         // sets the intent to pass the info to listdisplay activity
@@ -133,24 +195,25 @@ namespace nutr_grabber
                         foreach (var item in query)
                         {
                             // passes items to second activity, then launches the new activity
-                            second.PutExtra("name", Convert.ToString(item.Shrt_Desc));
-                            second.PutExtra("Kcal", Convert.ToString(item.Energ_Kcal));
-                            second.PutExtra("protein", Convert.ToString(item.Protein_g));
-                            second.PutExtra("fat", Convert.ToString(item.Lipid_Tot_g));
-                            second.PutExtra("carbs", Convert.ToString(item.Carbohydrt_g));
-                            second.PutExtra("sodium", Convert.ToString(item.Sodium_mg));
-                            second.PutExtra("sugar", Convert.ToString(item.Sugar_Tot_g));
-                            second.PutExtra("num", Convert.ToString(item.num));
-                            second.PutExtra("unit", item.unit);
+                            second.PutExtra("name", Convert.ToString(name));
+                            second.PutExtra("Kcal", Convert.ToString(cal));
+                            second.PutExtra("protein", Convert.ToString(protein));
+                            second.PutExtra("fat", Convert.ToString(fat));
+                            second.PutExtra("carbs", Convert.ToString(carbs));
+                            second.PutExtra("sodium", Convert.ToString(sodium));
+                            second.PutExtra("sugar", Convert.ToString(sugar));
+                            second.PutExtra("num", Convert.ToString(amountEntered));
+                            second.PutExtra("unit", unitEntered);
                             StartActivity(second);
 
                         }
+                        
                     };
         }
             
     
 
-//----------------------------------------------------------------------------
+//---------------------------------------------------------------------------------
        
         public class USDADataProto
         {
@@ -158,60 +221,56 @@ namespace nutr_grabber
             public int NDB_No { get; set; }
             [PrimaryKey]
             public string Shrt_Desc { get; set; }
-            public int Energ_Kcal { get; set; }           
-            public int Protein_g { get; set; }
-            public int Lipid_Tot_g { get; set; }
-            public int Ash_g { get; set; }
-            public int Carbohydrt_g { get; set; }
-            public int Fiber_TD_g { get; set; }
-            public int Sugar_Tot_g { get; set; }
-            public int Calcium_mg { get; set; }
-            public int Iron_mg { get; set; }
-            public int Magnesium_mg { get; set; }
-            public int Phosphorus_mg { get; set; }
-            public int Potassium_mg { get; set; }
-            public int Sodium_mg { get; set; }
-            public int Zinc_mg { get; set; }
-            public int Copper_mg { get; set; }
-            public int Manganese_mg { get; set; }
-            public int Selenium_ug { get; set; }
-            public int Vit_C_mg { get; set; }
-            public int Thiamin_mg { get; set; }
-            public int Riboflavin_mg { get; set; }
-            public int Niacin_mg { get; set; }
-            public int Panto_Acid_mg { get; set; }
-            public int Vit_B6_mg { get; set; }
-            public int Folate_Tot_ug { get; set; }
-            public int Folic_Acid_ug { get; set; }
-            public int Food_Folate_ug { get; set; }
-            public int Folate_DFE_ug { get; set; }
-            public int Choline_Tot_mg { get; set; }
-            public int Vit_B12_ug { get; set; }
-            public int Vit_A_IU { get; set; }
-            public int Vit_A_RAE { get; set; }
-            public int Retinol_ug { get; set; }
-            public int Alpha_Carot_ug { get; set; }
-            public int Beta_Carot_ug { get; set; }
-            public int Beta_Crypt_ug { get; set; }
-            public int Lycopene_ug { get; set; }
-            public int Lut_Zea_ug { get; set; }
-            public int Vit_E_mg { get; set; }
-            public int Vit_D_ug { get; set; }
-            public int Vit_D_IU { get; set; }
-            public int Vit_K_ug { get; set; }
-            public int FA_Sat_g { get; set; }
-            public int FA_Mono_g { get; set; }
-            public int FA_Poly_g { get; set; }
-            public int Cholestrl_mg { get; set; }
-            public int Gm_unit { get; set; }
-            public int num { get; set; }
+            public float Energ_Kcal { get; set; }           
+            public float Protein_g { get; set; }
+            public float Lipid_Tot_g { get; set; }
+            public float Ash_g { get; set; }
+            public float Carbohydrt_g { get; set; }
+            public float Fiber_TD_g { get; set; }
+            public float Sugar_Tot_g { get; set; }
+            public float Calcium_mg { get; set; }
+            public float Iron_mg { get; set; }
+            public float Magnesium_mg { get; set; }
+            public float Phosphorus_mg { get; set; }
+            public float Potassium_mg { get; set; }
+            public float Sodium_mg { get; set; }
+            public float Zinc_mg { get; set; }
+            public float Copper_mg { get; set; }
+            public float Manganese_mg { get; set; }
+            public float Selenium_ug { get; set; }
+            public float Vit_C_mg { get; set; }
+            public float Thiamin_mg { get; set; }
+            public float Riboflavin_mg { get; set; }
+            public float Niacin_mg { get; set; }
+            public float Panto_Acid_mg { get; set; }
+            public float Vit_B6_mg { get; set; }
+            public float Folate_Tot_ug { get; set; }
+            public float Folic_Acid_ug { get; set; }
+            public float Food_Folate_ug { get; set; }
+            public float Folate_DFE_ug { get; set; }
+            public float Choline_Tot_mg { get; set; }
+            public float Vit_B12_ug { get; set; }
+            public float Vit_A_IU { get; set; }
+            public float Vit_A_RAE { get; set; }
+            public float Retinol_ug { get; set; }
+            public float Alpha_Carot_ug { get; set; }
+            public float Beta_Carot_ug { get; set; }
+            public float Beta_Crypt_ug { get; set; }
+            public float Lycopene_ug { get; set; }
+            public float Lut_Zea_ug { get; set; }
+            public float Vit_E_mg { get; set; }
+            public float Vit_D_ug { get; set; }
+            public float Vit_D_IU { get; set; }
+            public float Vit_K_ug { get; set; }
+            public float FA_Sat_g { get; set; }
+            public float FA_Mono_g { get; set; }
+            public float FA_Poly_g { get; set; }
+            public float Cholestrl_mg { get; set; }
+            public float Gm_unit { get; set; }
+            public float num { get; set; }
             public string unit { get; set; }
-
-    
+  
         }
-
-
-
 
     }
 }
