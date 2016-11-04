acc7f4120a9fb3f36718bde50cdd90a6b9e764cb
slest003 <slest003@plattsburgh.edu>
2016-11-04 16:57:47 -0400

    Create QuickFRENCH.cs

---
+++ Android-App/QuickFRENCH.cs
@@ -0,0 +1,54 @@
+//Sets display for quick search layout. But in french.
+
+
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    android:orientation="vertical"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:minWidth="25px"
+    android:minHeight="25px">
+    <TextView
+        android:text="Inscrivez le montant:"
+        android:textAppearance="?android:attr/textAppearanceSmall"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:id="@+id/textAmount" />
+    <EditText
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:background="#4c4c4c"
+        android:id="@+id/enterAmount"
+        android:layout_marginBottom="9.0dp" />
+    <TextView
+        android:text="Entrer dans l'unité:"
+        android:textAppearance="?android:attr/textAppearanceSmall"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:id="@+id/textUnit" />
+    <Spinner
+        android:layout_width="match_parent"
+        android:layout_height="38.0dp"
+        android:id="@+id/unitSpinner"
+        android:layout_marginBottom="9.0dp"
+        android:background="#4c4c4c"
+        android:popupBackground="#4c4c4c"
+        android:tag="Unit" />
+    <TextView
+        android:text="Entrer l'ingrédient:"
+        android:textAppearance="?android:attr/textAppearanceSmall"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:id="@+id/textIngred" />
+    <AutoCompleteTextView
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:background="#4c4c4c"
+        android:id="@+id/autoCompIngred"
+        android:layout_marginBottom="9.0dp"/>
+    <Button
+        android:text="Chercher"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:id="@+id/search" />
+</LinearLayout
